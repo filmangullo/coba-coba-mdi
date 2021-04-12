1a52de387bc39d0a53009608ac052f9a051f2002
filmangullo <filmantapgullo@gmail.com>
2021-04-12 22:05:43 +0700

    finishing career

--- resources/views/career/index.blade.php
+++ resources/views/career/index.blade.php
@@ -15,8 +15,34 @@
 
 </div>
 
-<div class="grid grid-cols-1 px-5 mt-20 md:grid-cols-2">
+<div class="container mx-auto">
+    <div class="grid grid-cols-1 px-5 mt-20 mb-10 md:grid-cols-2">
+        <div class="mx-12">
+            <div class="text-2xl font-bold">
+                Maintener Head
+            </div>
+            <span class="text-xs">Senin, 28 Januari 2020</span>
+            <p class="my-5 text-justify">
+                PT Mark Dynamics Indonesia Tbk membutuhkan seorang profesional untuk bergabung sebagai Maintenance Manager yang bertanggung jawab untuk memastikan instalasi, perbaikan, serta operasional seluruh mesin dan fasilitas perusahaan untuk mendukung operasional...
+            </p>
+            <a href="" class="px-5 py-2 my-4 font-semibold text-white rounded-md bg-mark hover:bg-bright">
+            Read More
+            </a>
+        </div>
+    </div>
+</div>
 
+<div class="text-center bg-mark">
+    <div class="container py-20 mx-auto text-white">
+        <h1 class="text-4xl font-bold">Interested in working with us?</h1>
+        <p>Press the button below and fill the form to get the chance to work with us.</p>
+        <div class="py-4">
+            <a href="#"
+                class="p-2 font-semibold border-2 border-white rounded-lg">
+                Career Form
+            </a>
+        </div>
+    </div>
 </div>
 
 @endsection
