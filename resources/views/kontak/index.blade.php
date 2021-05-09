a80e87ab856eef692ebab3fc17d6f474950b7a8d
Core-Coder <jouwineliepangi@gmail.com>
2021-05-09 15:08:39 +0700

    done contact us review

--- resources/views/kontak/index.blade.php
+++ resources/views/kontak/index.blade.php
@@ -2,23 +2,16 @@
 
 @section('content')
 {{-- Slider --}}
-<div class="h-20">
-    Header
-</div>
-<div class="relative w-full h-60">
-    <div class="absolute left-0 right-0 z-20 w-full py-20 mx-auto my-auto space-y-6 text-center text-white">
-        <div class="text-5xl font-bold">Contact Us</div>
-
-    </div>
-    <div class="absolute w-full h-full bg-opacity-50 bg-mark"></div>
-    <img class="w-full h-full" src='{{ asset('img/header-contact.png') }}'>
-
+<div class="w-full h-auto relative pt-24">
+    <img src="{{ asset('img/header-contact.png') }}" alt="Mark Dynamics Contact Us">
+    <div class="absolute bg-mark-default bg-opacity-50 h-full top-0 w-full"></div>
+    <div class="absolute text-white text-center w-full text-4xl font-bold top-1/2 mt-5 h-full">Contact Us</div>
 </div>
 
-<div class="grid grid-cols-1 px-5 mt-20 md:grid-cols-2">
+<div class="grid grid-cols-1 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-24 md:grid-cols-2">
     <div>
         <div class="mx-8">
-            <div class="text-5xl font-bold">
+            <div class="text-4xl font-bold">
                 Contact Us
             </div>
             <span class="text-xs text-gray-800">Feel free to ask us anything, we’ll try our best to answer your question as fast as we could.</span>
@@ -27,14 +20,32 @@
     </div>
 
     <div>
-        <div class="p-10 mx-8 rounded-lg bg-mark">
-            <div class="text-3xl font-bold text-white">
+        <div class="p-10 mx-8 rounded-xl space-y-12 bg-mark-default">
+            <div class="text-2xl font-bold text-white">
                 Our Information
             </div>
-            <div class="mt-4 ml-6 font-semibold text-white">
-                Jl. Pelita Barat No. 2, Medan Star Industrial Estate, <br>
-                Jl. Raya Medan Lubuk Pakam Km. 19,5 <br>
-                North Sumatera – 20362, Indonesia
+            <div class="space-y-6">
+                <div class="flex items-center gap-4">
+                    <img class="w-6" src="{{ asset('icon/message.svg') }}" alt="Mark Dynamics Email">
+                    <div class="font-semibold text-white">mdcps@nusa.net.idmdcps@nusa.net.id</div>
+                </div>
+                <div class="flex items-center gap-4">
+                    <img class="w-6" src="{{ asset('icon/fax.svg') }}" alt="Mark Dynamics Fax">
+                    <div class="font-semibold text-white">(+62-61) 794-0747</div>
+                </div>
+                <div class="flex items-center gap-4">
+                    <img class="w-6" src="{{ asset('icon/phone.svg') }}" alt="Mark Dynamics Phone">
+                    <div class="font-semibold text-white">(+62-61) 794-0715</div>
+                </div>
+                <div class="flex items-start gap-4">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="text-white h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" />
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
+                      </svg>
+                    <div class="font-semibold text-white">Jl. Pelita Barat No. 2, Medan Star Industrial Estate, <br>
+                        Jl. Raya Medan Lubuk Pakam Km. 19,5 <br>
+                        North Sumatera – 20362, Indonesia</div>
+                </div>
             </div>
         </div>
     </div>
--- resources/views/livewire/web/kontak/form.blade.php
+++ resources/views/livewire/web/kontak/form.blade.php
@@ -1,28 +1,28 @@
 <div class="py-5">
     <form method="POST" action="action.php">
         <div class="mb-5">
-            <label class="font-semibold text-gray-900 text-md">Title</label></br>
-            <input type="text" class="w-full p-2 mt-1 border-2 border-gray-300 rounded-md" name="title" id="title" value="" required></input>
+            <label class="font-semibold text-gray-900 text-md">Name</label></br>
+            <input type="text" class="w-full p-2 text-sm mt-1 border focus:ring-0 focus:border-mark border-gray-500 rounded-md" name="title" id="title" value="" required></input>
         </div>
 
         <div class="mb-5">
             <label class="font-semibold text-gray-900 text-md">Email</label></br>
-            <input type="text" class="w-full p-2 mt-1 border-2 border-gray-300 rounded-md" name="title" id="title" value="" required></input>
+            <input type="text" class="w-full p-2 mt-1 border focus:ring-0 focus:border-mark text-sm border-gray-500 rounded-md" name="title" id="title" value="" required></input>
         </div>
 
         <div class="mb-5">
             <label class="font-semibold text-gray-900 text-md">Subject</label></br>
-            <input type="text" class="w-full p-2 mt-1 border-2 border-gray-300 rounded-md" name="title" id="title" value="" required></input>
+            <input type="text" class="w-full p-2 mt-1 border focus:ring-0 focus:border-mark text-sm border-gray-500 rounded-md" name="title" id="title" value="" required></input>
         </div>
 
         <div class="mb-5">
             <label class="font-semibold text-gray-900 text-md">Message</label></br>
-            <textarea class="w-full p-2 mt-1 border-2 border-gray-300 rounded-md"></textarea>
+            <textarea class="w-full h-20 p-2 mt-1 border focus:ring-0 focus:border-mark text-sm border-gray-500 rounded-md"></textarea>
         </div>
 
         <div class="mb-5">
-            <button class="w-full p-2 font-bold text-gray-600 uppercase border-2 rounded-lg hover:bg-mark hover:text-white border-mark">
-                S e n d
+            <button class="w-full p-3 text-mark-default font-bold transition-all duration-300 border-2 rounded-md hover:bg-mark-default hover:text-white border-mark">
+                Send
             </button>
         </div>
     </form>
