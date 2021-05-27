7a1bd4cbd329501a4b8f7ae0b3f1cf1f36cc79fc
Core-Coder <jouwineliepangi@gmail.com>
2021-05-27 19:52:40 +0700

    added chart js npm

--- resources/views/livewire/web/career/apply.blade.php
+++ resources/views/livewire/web/career/apply.blade.php
@@ -31,7 +31,6 @@
             <label class="block mb-2 text-sm font-bold text-gray-700" for="email">
               Email
             </label>
-            @dump($email)
             <input wire:model="email"
                 class="w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline" type="email" placeholder="Email">
         </div>
