2394565d47d29384cea7675be4077ffbc3ab5bfd
Sinji Prasetio <sinjiprasetio@outlook.com>
2021-05-29 20:32:45 +0700

    add career suvbmit function 50%

---
+++ resources/views/emails/career/mail.blade.php
@@ -0,0 +1,32 @@
+@component('mail::message')
+# Introduction
+
+
+Full Name : {{$details['name']}}
+<br>
+            
+Job Applied : {{$details['apply']}}
+<br>
+
+Phone : {{$details['phone']}}
+<br>
+
+Email : {{$details['email']}}
+<br>
+
+Address : {{$details['address']}}
+<br>
+
+About yourself : {{$details['about']}}
+<br>
+
+Why we should hire you? : {{$details['why']}}
+<br>
+
+@component('mail::button', ['url' => 'mailto:'. $details['email']])
+Reply Now
+@endcomponent
+
+Thanks,<br>
+{{ config('app.name') }}
+@endcomponent
