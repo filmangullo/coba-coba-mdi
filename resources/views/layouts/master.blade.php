aa94f33abad414f0bbacd41b977f56d036479299
Sinji Prasetio <sinjiprasetio@outlook.com>
2021-05-29 10:05:06 +0700

    fix career

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -12,7 +12,7 @@
 
         <!-- Styles -->
         <link rel="stylesheet" href="{{ mix('css/app.css') }}">
-
+        @livewireStyles
         <!-- Scripts -->
         <script src="{{ mix('js/app.js') }}" defer></script>
     </head>
@@ -254,6 +254,7 @@
             </div>
         </div>
 
+        @livewireScripts
         @stack('scripts')
     </body>
 </html>
