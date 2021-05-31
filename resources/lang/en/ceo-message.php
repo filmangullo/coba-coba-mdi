a2bc6c498c7fbe74b4843cef34d49a9ef8afb8d7
filmangullo <filmantapgullo@gmail.com>
2021-05-31 22:58:36 +0700

    sedikit lang

---
+++ resources/lang/en/ceo-message.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | CEO Message Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used by the paginator library to build
+    | the simple CEO Message links. You are free to change them to anything
+    | you want to CEO Messageize your views to better match your application.
+    |
+    */
+
+    'ceo-message'   => 'CEO Message',
+    'message'       => '<span class="ml-12">In</span> 2020, we have witnessed something beyond our imagination as the Coronavirus-19 hit the entire world. Damage inflicted by the pandemic has stunned many across the world. Lives were gone, jobs were lost, and businesses collapsed. I would like to express my deepest condolences to those who have been negatively impacted by the outbreak and to direct my utmost gratitude to the medical professionals who have risked everything to cure the world. With vaccines available now, I do believe that this pandemic is coming to an end, sooner or later. <br>
+                        <span class="ml-12">As</span> we can see, the pandemic has caused the demand of gloves surging to a level it has never reached before. The spike has forced us to move quickly as global supply of hand formers is facing a shortage. We took the chance to expand by adding a new machine with the capacity of 200,000 pieces per month in the mid-year 2020, and building a new factory armed with a capacity of 600,000 pieces per month at the end of the year. The new factory has commenced operation in the first quarter of 2021. <br>
+                        <span class="ml-12">We</span> believe that this expansion is strategic to meet the supply-demand imbalance that has been going on for months. Our goal does not only stop at achieving economic values, but also to be able to contribute to the whole world, especially during this pandemic. Last but not least, we hope that the pandemic will be over very soon.
+    ',
+    'january-2021'  => 'January 2021',
+    'president-director-CEO'    => 'President Director, CEO',
+    'ridwan-goh'    => 'Ridwan Goh'
+
+];
