38878163b081b153d7b7aea3d62accf0e6a36f95
Core-Coder <jouwineliepangi@gmail.com>
2021-06-04 22:18:07 +0700

    public expose

--- resources/views/investor/reports/public-expose.blade.php
+++ resources/views/investor/reports/public-expose.blade.php
@@ -4,7 +4,7 @@
 <div class="w-full h-auto relative pt-24">
     <img class="2xl:w-full" src="{{ asset('img/public-expose-hero.png') }}" alt="Mark Dynamics Awards & Certificates">
     <div class="absolute bg-mark-default bg-opacity-50 h-full top-0 w-full"></div>
-    <div class="absolute text-white text-center w-full text-3xl sm:text-4xl font-bold top-1/2 mt-5 h-full">Public Expose</div>
+    <div class="absolute text-white text-center w-full text-3xl sm:text-4xl font-bold top-1/2 mt-5 h-full">@lang('public-expose.title')</div>
 </div>
 <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 divide-y divide-hitam">
 <div class="space-y-24 py-24">
