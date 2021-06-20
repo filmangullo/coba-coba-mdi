c6c91b854ac1c2c426bb774b0941912dc681fa1d
filmangullo <filmantapgullo@gmail.com>
2021-06-20 23:06:48 +0700

    improvment lang csr & environment

--- resources/views/livewire/web/sustainability/environments.blade.php
+++ resources/views/livewire/web/sustainability/environments.blade.php
@@ -1,5 +1,5 @@
 <div class="px-4 pt-24 pb-12 mx-auto lg:pt-48 lg:pb-24 max-w-7xl sm:px-6 lg:px-8">
-    <div class="pb-12 -mb-2 text-2xl font-bold text-center lg:pb-24 lg:mb-0 lg:text-4xl">Environment</div>
+    <div class="pb-12 -mb-2 text-2xl font-bold text-center lg:pb-24 lg:mb-0 lg:text-4xl">@lang('custom.environment')</div>
     <div class="grid grid-cols-2 gap-4 mb-4 sm:grid-cols-4 lg:grid-cols-6">
         <div class="col-span-2 sm:col-span-4 lg:col-span-6 h-3/4">
             <img class="object-cover w-full rounded-md" src="{{ asset($nowPreview) }}" alt="Mark Dynamics Gallery & Products">
--- resources/views/sustainability/csr.blade.php
+++ resources/views/sustainability/csr.blade.php
@@ -4,7 +4,7 @@
 <div class="relative w-full h-auto pt-12 lg:pt-24">
     <img class="2xl:w-full" src="{{ asset('img/csr-hero.png') }}" alt="Mark Dynamics Awards & Certificates">
     <div class="absolute top-0 w-full h-full bg-opacity-50 bg-mark-default"></div>
-    <div class="absolute w-full h-full mt-5 text-sm font-bold text-center text-white sm:text-2xl lg:text-4xl top-1/2">CSR</div>
+    <div class="absolute w-full h-full mt-5 text-sm font-bold text-center text-white sm:text-2xl lg:text-4xl top-1/2">@lang('custom.csr')</div>
 </div>
 
 <div>
