0c2c823da11a69145d7b87f9eaf45ac8b75b9a84
filmangullo <filmantapgullo@gmail.com>
2021-06-20 20:47:05 +0700

    improvment csr

---
+++ app/Http/Livewire/Web/Sustainability/Csrs.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Livewire\Web\Sustainability;
+
+use Livewire\WithPagination;
+use Livewire\Component;
+use App\Models\Csr;
+
+class Csrs extends Component
+{
+    use WithPagination;
+    public $nowPreview;
+
+    public function mount()
+    {
+        if(!$this->nowPreview)
+        {
+            $this->nowPreview = Csr::orderBy('created_at', 'desc')->first();
+        }
+    }
+
+    public function preview($id)
+    {
+        $this->nowPreview = Csr::findOrFail($id);
+    }
+
+    public function render()
+    {
+        $csr =  Csr::orderBy('created_at', 'desc')->paginate(10);
+
+        return view('livewire.web.sustainability.csrs', [
+            'csr' => $csr
+        ]);
+    }
+}
--- resources/views/sustainability/csr.blade.php
+++ resources/views/sustainability/csr.blade.php
@@ -6,17 +6,9 @@
     <div class="absolute top-0 w-full h-full bg-opacity-50 bg-mark-default"></div>
     <div class="absolute w-full h-full mt-5 text-sm font-bold text-center text-white sm:text-2xl lg:text-4xl top-1/2">CSR</div>
 </div>
-<div class="px-4 py-6 mx-auto divide-y-2 divide-gray-200 lg:py-12 max-w-7xl sm:px-6 lg:px-8">
-    @foreach ($csr as $item)
-        <div class="py-6 lg:py-12">
-            <div class="text-sm font-bold uppercase text-mark-default sm:text-xl lg:text-3xl">{{ $item->title_id }}</div>
-            <div class="mt-1 text-xs italic font-bold text-gray-600 sm:mt-3 sm:text-sm lg:text-base uppecase">{{ $item->title_en }}</div>
-            <div class="mt-6 text-xs tracking-wide lg:mt-12 lg:text-sm">{!! $item->description_id !!}</div>
-            <div class="mt-6 text-xs italic tracking-wide text-right lg:mt-12 lg:text-sm">{!! $item->description_en !!}</div>
-            <img class="object-cover h-48 mx-auto mt-6 sm:h-96 lg:h-screen lg:mt-12" src="{{ asset($item->img) }}" alt="Mark Dynamics CSR">
-        </div>
-    @endforeach
 
-
+<div>
+    @livewire('web.sustainability.csrs')
 </div>
+
 @endsection
