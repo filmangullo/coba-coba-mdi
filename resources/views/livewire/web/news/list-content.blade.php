f0c0f2c6bcdcae0a48c6a0af380a1ac690918d5f
Core-Coder <jouwineliepangi@gmail.com>
2021-07-05 21:08:10 +0700

    news redesign done

--- resources/views/livewire/web/news/list-content.blade.php
+++ resources/views/livewire/web/news/list-content.blade.php
@@ -1,17 +1,17 @@
 <div>
-    <div class="items-start w-full grid-cols-2 gap-12 lg:grid">
+    <div class="items-start w-full grid-cols-2 gap-6 lg:grid">
         @forelse ($news as $key => $item)
         @if ($key == 0)
         <div class="relative w-full h-screen col-span-2">
             <div class="absolute z-10 w-11/12 pl-8 space-y-6 bottom-12 sm:pl-16">
-                <div class="text-4xl font-bold text-white xsm:text-6xl">
+                <div class="text-4xl font-bold text-white xsm:text-6xl line-clamp-3">
                     @if (__('custom.lang') == 'id')
                     {!! $item->title_id !!}
                     @else
                     {!! $item->title_en !!}
                     @endif
                 </div>
-                <div class="text-lg text-justify text-white ">
+                <div class="text-lg text-justify text-white line-clamp-5">
                     @if (__('custom.lang') == 'id')
                     {{ Str::limit($item->brief_description_id, 250, '...') }}
                     @else
@@ -28,7 +28,7 @@
             <img class="object-cover w-full h-full" src="{{ asset($item->cover) }}" alt="Mark Dynamics News">
         </div>
         @else
-        <div class="w-full px-4 mt-12 rounded-md lg:container lg:mx-auto lg::mt-0 sm:px-6 lg:px-8">
+        <div class="w-full rounded-md @if($key % 2 != 0) lg:pl-6 px-6 pb-6 lg:px-0 lg:pb-0 @else lg:pr-6 px-6 pb-6 lg:px-0 lg:pb-0 @endif @if($key == 1) lg:p-0 p-6 @endif lg:mx-auto">
             <div class="relative w-full mx-auto overflow-hidden rounded-md h-60 ">
                 <img class="object-cover w-full h-full" src="{{ asset($item->cover) }}" alt="Mark Dynamics News">
                 <div class="absolute top-0 w-full h-full bg-opacity-50 rounded-md bg-hitam"></div>
@@ -41,11 +41,11 @@
                         @endif
                     </div>
                     <div class="grid items-center grid-cols-5 gap-4 text-base font-light">
-                        <div class="justify-center w-24 h-20 py-2 text-center rounded-md bg-mark-default">
+                        <div class="hidden sm:block justify-center w-24 h-20 py-2 text-center rounded-md bg-mark-default">
                             <span class="text-sm">{{ date('M', strtotime($item->created_at)) }}</span><br>
                             <span class="text-3xl font-semibold">{{ date('d', strtotime($item->created_at)) }}</span>
                         </div>
-                        <div class="col-span-4 text-justify">
+                        <div class="col-span-5 sm:col-span-4 line-clamp-3">
                             @if (__('custom.lang') == 'id')
                             {{ Str::limit($item->brief_description_id, 108, '...') }}
                             @else
@@ -54,7 +54,7 @@
                         </div>
 
                     </div>
-                    <div class="flex justify-end item-right">
+                    <div class="flex justify-start sm:justify-end item-right">
                         <a href="{{ route('news.show', $item->slug )}}"
                             class="bottom-0 inline-block float-right px-3 py-2 font-bold truncate transition-all duration-200 border border-white rounded-md cursor-pointer sm:text-lg xsm:text-sm text-md hover:bg-gray-50 hover:text-hitam">Baca
                             Selengkapnya</a>
@@ -65,7 +65,7 @@
         @endif
         @empty
         <div class="relative w-full">
-
+            There's no news for now.
         </div>
         @endforelse
     </div>
--- resources/views/news/show.blade.php
+++ resources/views/news/show.blade.php
@@ -6,7 +6,11 @@
         <div class="lg:grid grid-cols-12 gap-12">
             <div class="col-span-8">
                 <div class="mb-6 lg:mb-0">
+                    @if(App::isLocale('id'))
                     <h1 class="pb-4 text-2xl lg:text-4xl font-bold">{{ $news->title_id }}</h1>
+                    @elseif(App::isLocale('en'))
+                    <h1 class="pb-4 text-2xl lg:text-4xl font-bold">{{ $news->title_en }}</h1>
+                    @endif
                     <div class="grid grid-cols-2 mb-4">
                         <div class="py-1 text-left text-gray-500 items-left">
                             <img class="inline-block w-4 opacity-60" src="{{ asset('icon/calendar-interface-symbol-tool.svg') }}">
@@ -18,7 +22,11 @@
                         </div>
                     </div>
                     <div class="py-1 lg:text-lg text-justify">
+                        @if(App::isLocale('id'))
                         {!! $news->content_id !!}
+                        @elseif(App::isLocale('en'))
+                        {!! $news->content_en !!}
+                        @endif
                     </div>
                 </div>
             </div>
@@ -29,8 +37,16 @@
                         @foreach ($list as $item)
                         <a href="{{ route('news.show', $item->slug )}}">
                             <div class="p-6 border border-gray-300 rounded-xl space-y-2">
+                                @if(App::isLocale('id'))
                                 <h5 class="font-semibold cursor-pointer line-clamp-2">{{ $item->title_id }}</h5>
+                                @elseif(App::isLocale('en'))
+                                <h5 class="font-semibold cursor-pointer line-clamp-2">{{ $item->title_en }}</h5>
+                                @endif
+                                @if(App::isLocale('id'))
                                 <p class="text-gray-600 text-sm">{{ Str::limit($item->brief_description_id, 100, '...') }}</p>
+                                @elseif(App::isLocale('en'))
+                                <p class="text-gray-600 text-sm">{{ Str::limit($item->brief_description_en, 100, '...') }}</p>
+                                @endif
                                 <div class="py-1 text-left text-gray-500 items-left">
                                     <img class="inline-block w-4 opacity-60" src="{{ asset('icon/calendar-interface-symbol-tool.svg') }}">
                                     <span class="ml-2 text-xs font-semibold text-gray-500">{{ date("d M Y", strtotime($item->news_date)) }}</span>
