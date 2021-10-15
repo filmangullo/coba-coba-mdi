d4ed9eb92a2f4b9aa49268540255ec09ae5dd208
filmangullo <filmantapgullo@gmail.com>
2021-10-15 13:56:44 +0700

    update run Prod

--- app/Http/Livewire/Web/ProductGallery/ListGallery.php
+++ app/Http/Livewire/Web/ProductGallery/ListGallery.php
@@ -11,7 +11,7 @@ class ListGallery extends Component
     public function render()
     {
         return view('livewire.web.product-gallery.list-gallery', [
-            'gallery'       => Gallery::where('is_product', false)->paginate(8)
+            'gallery'       => Gallery::where('is_product', false)->orderBy('created_at', 'desc')->paginate(8)
         ]);
     }
 }
