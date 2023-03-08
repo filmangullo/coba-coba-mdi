b0ddd9a270f6e599823edcc21be602245aa8eb0f
filmangullo <filmantapgullo@gmail.com>
2023-03-08 12:38:54 +0700

    test update

--- resources/views/investor/reports/dividend.blade.php
+++ resources/views/investor/reports/dividend.blade.php
@@ -10,11 +10,11 @@
     </p> --}}
         <div class="w-full mt-12 lg:mt-24">
             @if (App::isLocale('id'))
-                <img class="w-full" src="{{ asset('img/deviden.jpg') }}" alt="Mark Dynamics Dividend Information">
+                <img class="w-full" src="{{ asset('img/deviden.png') }}" alt="Mark Dynamics Dividend Information">
             @elseif(App::isLocale('en'))
-                <img class="w-full" src="{{ asset('img/deviden-eng.jpg') }}" alt="Mark Dynamics Dividend Information">
+                <img class="w-full" src="{{ asset('img/deviden-eng.png') }}" alt="Mark Dynamics Dividend Information">
             @elseif(App::isLocale('cn'))
-                <img class="w-full" src="{{ asset('img/deviden-cn.jpg') }}" alt="Mark Dynamics Dividend Information">
+                <img class="w-full" src="{{ asset('img/deviden-cn.png') }}" alt="Mark Dynamics Dividend Information">
             @endif
         </div>
     </div>
